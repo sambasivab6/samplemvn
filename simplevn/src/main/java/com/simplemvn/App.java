9b59caf980b24e7e3224ee790c662c46ed623467
sambasivab6 <sivarao.yarramsetty1@gmail.com>
2021-07-10 23:37:23 -0600

    First Commit

---
+++ simplevn/src/main/java/com/simplemvn/App.java
@@ -0,0 +1,13 @@
+package com.simplemvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
